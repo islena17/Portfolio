28b9531c8850052c12db9ed027e0bcef9674ca94
Islena <islenapolo17@gmail.com>
2026-06-20 18:44:08 +0200

    cambios 5

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/islena17.github.io/",
+  plugins: [react()]
 });
